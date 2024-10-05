1583fca105c890550d6dd63220667c7334476375
Andres Oliveira <andresoliveira@protonmail.com>
2024-10-05 12:31:15 -0300

    refactor: renderTaskList with DOM create element

--- main.js
+++ main.js
@@ -30,15 +30,75 @@ const addTask = () => {
 }
 
 const deleteTask = (e) => {
-  e.preventDefault()
   alert('delete')
 }
 
 const completeTask = (e) => {
-  e.preventDefault()
   alert('complete')
 }
 
 const renderTaskList = () => {
-  
+  const todoListElement = document.getElementById("todoList")
+
+  todoListElement.innerHTML = ""
+
+  todoList.forEach(todo => {
+    const li = document.createElement("li")
+    li.setAttribute("id", todo.id)
+    li.classList.add(
+      "list-group-item",
+      "mb-2"
+    )
+
+    const div = document.createElement("div")
+    div.classList.add(
+      "d-flex",
+      "align-items-center",
+      "justify-content-between",
+    )
+
+    const label = document.createElement("label")
+    label.textContent = todo.text
+
+    const div2 = document.createElement("div")
+    div2.classList.add(
+      "btn-group",
+      "flex",
+      "align-items-center",
+      "justify-content-center",
+      "gap-1"
+    )
+
+    const completeButton = document.createElement("i")
+    completeButton.classList.add(
+      "fa-solid",
+      "fa-circle-check",
+      "text-success"
+    )
+    
+    completeButton.addEventListener("click", (e) => {
+      e.preventDefault()
+      completeTask(todo)
+    })
+
+    const deleteButton = document.createElement("i")
+    deleteButton.classList.add(
+      "fa-solid",
+      "fa-circle-minus",
+      "text-danger"
+    )
+
+    deleteButton.addEventListener("click", (e) => {
+      e.preventDefault()
+      deleteTask(todo)
+    })
+
+    todoListElement.appendChild(li)
+    li.appendChild(div)
+    div.appendChild(label)
+    div.appendChild(div2)
+    div2.appendChild(completeButton)
+    div2.appendChild(deleteButton)
+    
+  });
 }
