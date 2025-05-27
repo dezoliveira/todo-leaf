6e028bbb0e3435ebb71248e8164f78074be27276
Andres Oliveira <andresoliveira@protonmail.com>
2025-05-26 21:01:22 -0300

    add: comments

--- scripts/utils/pagination.js
+++ scripts/utils/pagination.js
@@ -1,11 +1,15 @@
 import { renderTaskList } from "../tasks/render"
 
+// Cria a paginação
 export const createPagination = (tasks) => {
-  // pagination
+  // elemento paginação
   const pagination = document.querySelector('#pagination')
+  
+  // calcula o número de páginas com base no número de tarefas
   const pages = Math.ceil(tasks.length / 10)
-  let html = '<ul class="d-flex pagination">'
+  let html = '<ul class="pagination d-flex justify-content-end m-0 p-2">'
 
+  // exibe apenas se tiver alguma página
   if (pages <= 1) {
     pagination.style.display = 'none'
     return
@@ -13,6 +17,7 @@ export const createPagination = (tasks) => {
 
   pagination.style.display = 'block'
 
+  // renderiza lista de páginas
   for (let i = 1; i <= pages; i++) {
     html += `<a class="page-link" data-page="${i}"><li class="page-item">${i}</li></a>`
   }
@@ -20,12 +25,12 @@ export const createPagination = (tasks) => {
   html += '</ul>'
   pagination.innerHTML = html
 
+  // adiciona evento de clique nas páginas e enviar página clicada
   document.querySelectorAll('.page-link').forEach(link => {
     link.addEventListener('click', (e) => {
       e.preventDefault()
 
       const page = parseInt(e.currentTarget.getAttribute('data-page'))
-      console.log(page)
       renderTaskList(page)
     })
   })
